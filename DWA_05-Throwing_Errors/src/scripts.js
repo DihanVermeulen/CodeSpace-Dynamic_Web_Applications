9127edca1576dce8b5537d5f4d6a57113f030143
Dihan_Vermeulen <dihan.vermeulen12@gmail.com>
2023-04-24 13:13:46 +0200

    Checks if elements exists and if not throw error

--- DWA_05-Throwing_Errors/src/scripts.js
+++ DWA_05-Throwing_Errors/src/scripts.js
@@ -1,11 +1,29 @@
 // @ts-check
 
+/**
+ * Form from html
+ * @type {Element | null}
+ */
 const form = document.querySelector("[data-form]");
+/**
+ * Div element from html
+ * @type {HTMLDivElement | null}
+ */
 const result = document.querySelector("[data-result]");
+if (!form) {
+  throw new Error();
+} else {
+  form.addEventListener("submit", (event) => {
+    if (!event) throw new Error();
+    event.preventDefault();
 
-form.addEventListener("submit", (event) => {
-  event.preventDefault();
-  const entries = new FormData(event.target);
-  const { dividend, divider } = Object.fromEntries(entries);
-  result.innerText = dividend / divider;
-});
+    /**
+     * Entries derived from submit event
+     */
+    const entries = new FormData(event.target);
+    const { dividend, divider } = Object.fromEntries(entries);
+    if (result) {
+      result.innerText = (dividend / divider).toString();
+    }
+  });
+}
