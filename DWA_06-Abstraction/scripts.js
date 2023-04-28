b7afd258c701ecef86d38a8e99e758c67910e48a
Dihan_Vermeulen <dihan.vermeulen12@gmail.com>
2023-04-28 06:43:12 +0200

    Moved to separate files

--- DWA_06-Abstraction/scripts.js
+++ DWA_06-Abstraction/scripts.js
@@ -7,63 +7,6 @@ if (!document) throw new Error();
 
 let page = 1;
 let matches = books;
-
-/**
- * All element selectors
- */
-const elementSelectors = {
-  /**
-   * Elements inside of header
-   */
-  header: {
-    headerSearch: "[data-header-search]",
-    headerSettings: "[data-header-settings]",
-  },
-  /**
-   * Elements inside of list
-   */
-  list: {
-    listItems: "[data-list-items]",
-    listButton: "[data-list-button]",
-    listMessage: "[data-list-message]",
-    listClose: "[data-list-close]",
-    listActive: "[data-list-active]",
-    listBlur: "[data-list-blur]",
-    listImage: "[data-list-image]",
-    listTitle: "[data-list-title]",
-    listDescription: "[data-list-description]",
-    listSubtitle: "[data-list-subtitle]",
-  },
-  /**
-   * Elements inside of search
-   */
-  search: {
-    searchGenres: "[data-search-genres]",
-    searchAuthors: "[data-search-authors]",
-    searchOverlay: "[data-search-overlay]",
-    searchCancel: "[data-search-cancel]",
-    searchForm: "[data-search-form]",
-    searchTitle: "[data-search-title]",
-  },
-  /**
-   * Elements inside of settings
-   */
-  settings: {
-    settingsCancel: "[data-settings-cancel]",
-    settingsOverlay: "[data-settings-overlay]",
-    settingsForm: "[data-settings-form]",
-  },
-};
-
-/**
- * Toggles the overlay based on the state passed in
- * @param {string} selector
- * @param {boolean} state
- */
-const toggleOverlay = (selector, state) => {
-  document.querySelector(selector).open = state;
-};
-
 const starting = document.createDocumentFragment();
 
 /*
