13b13ef7f34d1a10469addb160181b95849ed174
Dihan_Vermeulen <dihan.vermeulen12@gmail.com>
2023-05-01 09:10:09 +0200

    Updated import

--- DWA_08-Encapsulation/scripts.js
+++ DWA_08-Encapsulation/scripts.js
@@ -2,7 +2,7 @@ import { authors, genres } from "./src/data.js";
 import createBookListHtml from "./src/components/BookList.js";
 import createSearchHtml from "./src/components/Search.js";
 import state from "./src/state.js";
-import initiateSettings from "./src/helpers/settings.js";
+import initiateSettings from "./src/components/Settings.js";
 import initiateHeader from "./src/helpers/header.js";
 
 if (!document) throw new Error();
