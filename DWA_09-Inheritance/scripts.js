cac1e6d0fe70a1a37a89d2edc3653f58550734df
Dihan_Vermeulen <dihan.vermeulen12@gmail.com>
2023-05-03 16:58:12 +0200

    Imports book-preview.js file

--- DWA_09-Inheritance/scripts.js
+++ DWA_09-Inheritance/scripts.js
@@ -4,6 +4,7 @@ import createSearchHtml from "./src/components/Search.js";
 import state from "./src/state.js";
 import initiateSettings from "./src/components/Settings.js";
 import initiateHeader from "./src/helpers/header.js";
+import "./src/components/book-preview.js";
 
 if (!document) throw new Error();
 (function setup() {
