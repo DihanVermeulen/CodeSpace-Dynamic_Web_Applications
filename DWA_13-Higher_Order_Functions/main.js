a33f7c9e68b3b2533deda9cefe7f0b5efc5dcb47
Dihan_Vermeulen <dihan.vermeulen12@gmail.com>
2023-05-10 14:24:34 +0200

    Completed fourth challenge

--- DWA_13-Higher_Order_Functions/main.js
+++ DWA_13-Higher_Order_Functions/main.js
@@ -40,17 +40,24 @@ names.forEach((name, index) => {
   console.log(`${name} (${provinces[index]})`);
 });
 
-/* Challenge 2
+/* Challenge 3
  * Using map loop over all province names and turn the string to all uppercase.
  * Log the new array to the console.
  */
 
 console.log("Challenge 3 \n===================");
 
-const upperCaseProvinces = provinces.map((province) => {
-  return province.toUpperCase();
-});
+const upperCaseProvinces = provinces.map((province) => province.toUpperCase());
 
 console.log(upperCaseProvinces);
 
+/* Challenge 4
+ * Create a new array with map that has the amount of characters in each name.
+ * The result should be: [6, 9, 11, 5, 8, 7, 7]
+ */
 
+console.log("Challenge 4 \n===================");
+
+const amountOfCharactersInName = names.map((name) => name.length);
+
+console.log(amountOfCharactersInName);
