56993774056cea965137a3eac2a6aec0f658291d
Dihan_Vermeulen <dihan.vermeulen12@gmail.com>
2023-05-10 14:22:09 +0200

    Completed third challenge

--- DWA_13-Higher_Order_Functions/main.js
+++ DWA_13-Higher_Order_Functions/main.js
@@ -29,13 +29,28 @@ names.forEach((name) => {
   console.log(name);
 });
 
-console.log("Challenge 2 \n===================");
-
 /* Challenge 2
  * Use forEach to console log each name with a matching province (for example Ashwin (Western Cape).
  * Note that you are only allowed to call console.log seven times.
  */
 
+console.log("Challenge 2 \n===================");
+
 names.forEach((name, index) => {
   console.log(`${name} (${provinces[index]})`);
 });
+
+/* Challenge 2
+ * Using map loop over all province names and turn the string to all uppercase.
+ * Log the new array to the console.
+ */
+
+console.log("Challenge 3 \n===================");
+
+const upperCaseProvinces = provinces.map((province) => {
+  return province.toUpperCase();
+});
+
+console.log(upperCaseProvinces);
+
+
