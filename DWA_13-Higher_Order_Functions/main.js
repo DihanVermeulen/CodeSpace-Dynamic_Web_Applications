1e47f38498c6d754e8de6d0b23cae89b50cd75c5
Dihan_Vermeulen <dihan.vermeulen12@gmail.com>
2023-05-10 14:19:19 +0200

    Completed challenge 2

--- DWA_13-Higher_Order_Functions/main.js
+++ DWA_13-Higher_Order_Functions/main.js
@@ -24,6 +24,18 @@ const names = [
  * Use forEach to console log each name to the console. You are allowed to call console.log seven times.
  */
 console.log("Challenge 1 \n===================");
+
 names.forEach((name) => {
   console.log(name);
+});
+
+console.log("Challenge 2 \n===================");
+
+/* Challenge 2
+ * Use forEach to console log each name with a matching province (for example Ashwin (Western Cape).
+ * Note that you are only allowed to call console.log seven times.
+ */
+
+names.forEach((name, index) => {
+  console.log(`${name} (${provinces[index]})`);
 });
